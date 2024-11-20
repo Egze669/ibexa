c67f19aa4d4d9fcddf9d54d5683b6eb951b7e809
mwalkowiak <mwalkowiak@kaliop.com>
2024-11-20 10:01:49 +0100

    html fix, simplified controller

--- src/Controller/DealershipController.php
+++ src/Controller/DealershipController.php
@@ -23,31 +23,22 @@ class DealershipController extends Controller
      * @throws BadStateException
      */
     public function index(Repository $repository, View $view): View
- {
-     $rootLocation = $this->getRootLocation();
-     $contentFilter = new Filter();
-     $contentFilter
-         ->withCriterion(new Criterion\Visibility(Criterion\Visibility::VISIBLE))
-         ->andWithCriterion(new Criterion\Subtree($rootLocation->pathString))
-         ->andWithCriterion(new Criterion\ContentTypeIdentifier(['car']));
+    {
+        $rootLocation = $this->getRootLocation();
+        $locationList = [];
+        $locationFilter = new Filter();
+        $locationFilter
+            ->withCriterion(new Criterion\Visibility(Criterion\Visibility::VISIBLE))
+            ->andWithCriterion(new Criterion\Subtree($rootLocation->pathString))
+            ->andWithCriterion(new Criterion\ContentTypeIdentifier(['car']));
 
-     $response = $repository->getContentService()->find($contentFilter,[]);
-    $locationList = [];
-     foreach ($response as $item) {
-         $locationFilter = new Filter();
-         $locationFilter
-             ->withCriterion(new Criterion\ContentId($item->getId()))
-             ->andWithCriterion(new Criterion\Subtree($rootLocation->pathString));
-
-            foreach ($repository->getLocationService()->find($locationFilter) as $location) {
-                $id = $item->getId();
-                $locationList[$id] = $location;
-            }
-     }
-     $view->addParameters([
-         'carLocations' => $locationList,
-         'cars' => $response
-     ]);
-     return $view;
- }
+        $response = $repository->getLocationService()->find($locationFilter);
+        foreach ($response as $location) {
+            $locationList[$location->getContentId()] = $location;
+        }
+        $view->addParameters([
+            'cars' => $locationList,
+        ]);
+        return $view;
+    }
 }
